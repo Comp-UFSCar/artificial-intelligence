b8b02cd6b175fb9a5ddf211970753387c12c879f
Lucas David <lucasolivdavid@gmail.com>
2014-11-18 00:07:39 -0200

    Update source.pl

--- source.pl
+++ source.pl
@@ -1,16 +1,14 @@
-/**
- * source.pl
- *  
- * Describes a solution for the dust cleaner 
- * problem in an 2x2 space using A* algorithm.
- *
- * @author lucasdavid
- *
- */
+%
+% source.pl
+%  
+% Describes a solution for the dust cleaner problem in an 2x2 space using A* algorithm.
+%
+% @author lucasdavid
+%
 
 best_first([], _ , _ )
 :-
-    write('Solução não encontrada'), nl
+    write('Solucao nao encontrada'), nl
 .
 
 best_first([[_, G, _, _]|T], C, [_, G, _, _])
@@ -19,7 +17,7 @@ best_first([[_, G, _, _]|T], C, [_, G, _, _])
     write('open: '), printlist([G|T]), nl,
     write('closed: '), printlist(C), nl,
     write('OBJETIVO: '), write(G), nl,
-    write('Solução encontrada'), nl
+    write('Solucao encontrada'), nl
 .
 
 %# best_first([[4, [0, 1, 1, 1, 1], 0, 4]], [], [_,[0, 0, 0, 0, 0],_,_])
@@ -47,18 +45,18 @@ moves([F, S, D, H], T, C, [NF, A, NG, NH])
 printlist([]).
 printlist([[AgentsPos, S0, S1, S2, S3] | T])
 :-
-    write('  Agent\'s position: '), write(AgentsPos),
-    write('  Squares: '), write(S0), write(S1), write(S2), write(S3)
+    write('Agent\'s position:'), write(AgentsPos), write(' '),
+    write('Squares:'), write(S0), write(S1), write(S2), write(S3)
 .
 
-/**
- *
- * Returns NewState, a new generated state from a current state @State.
- * 
- * @param State
- * @param out NewState
- *
- */
+%
+%
+% Returns NewState, a new generated state from a current state @State.
+% 
+% @param State
+% @param out NewState
+%
+%
 %# Right movement
 %# Agent's in square [0, 0] or [1, 0]
 move([AgentsPos, S0, S1, S2, S3], [AgentsPos +1, S0, S1, S2, S3])
@@ -79,37 +77,37 @@ move([AgentsPos, S0, S1, S2, S3], [AgentsPos -1, S0, S1, S2, S3])
 move([AgentsPos, S0, S1, S2, S3], [AgentsPos -2, S0, S1, S2, S3])
     :- AgentsPos = 2; AgentsPos = 3.
 
-/**
- *
- * Returns G(State), where G is the function cost on a given state @State.
- * 
- * @param State
- * @param out G
- *
- */
+%
+%
+% Returns G(State), where G is the function cost on a given state @State.
+% 
+% @param State
+% @param out G
+%
+%
 %# When the agent has moved...
 calculaG([AgentsPos, S0, S1, S2, S3], [NewAgentsPos, S0, S1, S2, S3], InitialCost, NewCost)
 :-
     AgentsPos \= NewAgentsPos,
-    G is InitialCost + abs(AgentsPos - NewAgentsPos)
+    NewCost is InitialCost + abs(AgentsPos - NewAgentsPos)
 .
 
 %# When the agent has cleaned
 calculaG([AgentsPos, S0, S1, S2, S3], [AgentsPos, NewS0, NewS1, NewS2, NewS3], InitialCost, NewCost)
 :-
     (S0 \= NewS0 ; S1 \= NewS1 ; S2 \= NewS2 ; S3 \= NewS3),
-    G is InitialCost + 3
+    NewCost is InitialCost + 3
 .
 
-/**
- *
- * Calculates the heuristic function H.
- * H(State) = n, where @n is the number of unclean squares in a given state @State.
- * 
- * @param State
- * @param out F
- *
- */
+%
+%
+% Calculates the heuristic function H.
+% H(State) = n, where @n is the number of unclean squares in a given state @State.
+% 
+% @param State
+% @param out F
+%
+%
 calculaH([_|Squares], H)
     :- innerCalculaH(Squares, H) .
 
@@ -119,5 +117,5 @@ innerCalculaH([], H)
 innerCalculaH([Square | T], H)
 :-
     innerCalculaH(T, InnerH),
-    (Square = 1, H = InnerH + 1 ; Square = 0 , H = InnerH)
+    (Square = 1, H is InnerH + 1 ; Square = 0 , H is InnerH)
 .
